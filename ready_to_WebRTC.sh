0dfc9e6413f02a85963a9839d2bbfabc38c43160
dnjstjr93 <dnjstjr93@gmail.com>
2021-04-01 14:03:27 +0900

    Initial commit

---
+++ ready_to_WebRTC.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+wget http://ports.ubuntu.com/pool/universe/c/chromium-browser/chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
+sudo dpkg -i chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
