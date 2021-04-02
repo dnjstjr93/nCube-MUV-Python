08a0bdb1012a323a3f0a41a39b44692afa9b954f
dnjstjr93 <dnjstjr93@gmail.com>
2021-04-02 06:02:31 +0100

    - update tx2 board

--- ready_to_WebRTC.sh
+++ ready_to_WebRTC.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/sh
 
-wget http://ports.ubuntu.com/pool/universe/c/chromium-browser/chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
-sudo dpkg -i chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
+CHECK_OS="`cat /etc/issue | head -n 1`"
+OS="${CHECK_OS%% *}"
+
+echo  ${OS}
+
+if [[ "$OS" == "Raspbian" ]]; then
+	wget http://ports.ubuntu.com/pool/universe/c/chromium-browser/chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
+	sudo dpkg -i chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
+elif [[ "$OS" == "Linux" ]]; then
+	sudo apt-get purge -y chromium-browser
+	sudo apt-get install -y chromium-browser
+	sudo apt-get install -y chromium-chromedriver
