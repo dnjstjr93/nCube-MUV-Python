a2e9dc259d568df8bd09e22bb1e82203a49d6c05
dnjstjr93 <dnjstjr93@gmail.com>
2021-04-02 06:09:19 +0000

    - edit if

--- ready_to_WebRTC.sh
+++ ready_to_WebRTC.sh
@@ -5,10 +5,11 @@ OS="${CHECK_OS%% *}"
 
 echo  ${OS}
 
-if [[ "$OS" == "Raspbian" ]]; then
+if [ "${OS}" = "Raspbian" ]; then
 	wget http://ports.ubuntu.com/pool/universe/c/chromium-browser/chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
 	sudo dpkg -i chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
-elif [[ "$OS" == "Linux" ]]; then
+elif [ "${OS}" = "Ubuntu" ]; then
 	sudo apt-get purge -y chromium-browser
 	sudo apt-get install -y chromium-browser
 	sudo apt-get install -y chromium-chromedriver
+fi
