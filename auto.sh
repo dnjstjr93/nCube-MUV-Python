fc3e2c10c8be009d8f43d372c6aa1a12157adf4c
dnjstjr93 <dnjstjr93@gmail.com>
2021-04-05 05:11:09 +0100

    - edit command

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
-sudo chmod 777 /home/pi/nCube-MUV-Python/
-cd /home/pi/nCube-MUV-Python
+sudo chmod 777 /home/pi/nCube-MUV-python/
+cd /home/pi/nCube-MUV-python
 sudo chmod 777 *
 python3 thyme.py &
 
--- ready_to_WebRTC.sh
+++ ready_to_WebRTC.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/sh
 
-CHECK_OS="`cat /etc/issue | head -n 1`"
-OS="${CHECK_OS%% *}"
+OS="`uname -a`"
+CHECK_OS="`echo $OS | cut -d" " -f1`"
 
-echo  ${OS}
+echo  ${CHECK_OS}
 
-if [ "${OS}" = "Raspbian" ]; then
-	wget http://ports.ubuntu.com/pool/universe/c/chromium-browser/chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
-	sudo dpkg -i chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
-elif [ "${OS}" = "Ubuntu" ]; then
+if [ "${OS}" = "Darwin" ]; then
+	/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome -version
+	brew install -cask chromedriver
+	sudo spctl --master-disable 
+else
 	sudo apt-get purge -y chromium-browser
 	sudo apt-get install -y chromium-browser
 	sudo apt-get install -y chromium-chromedriver
