6f224054dfae888f51b7ba53c2d63108fd79f07b
dnjstjr93 <dnjstjr93@gmail.com>
2021-04-08 23:00:56 +0900

    - change single quotes to double quotes

--- conf.py
+++ conf.py
@@ -15,19 +15,19 @@ cnt_arr = []
 sub_arr = []
 acp = {}
 
-conf['useprotocol'] = 'http'  # select one for 'http' or 'mqtt' or 'coap' or 'ws'
+conf["useprotocol"] = 'http'  # select one for 'http' or 'mqtt' or 'coap' or 'ws'
 
 # build cse
 
 approval_host = dict()
-approval_host['ip'] = '203.253.128.177'
+approval_host["ip"] = '203.253.128.177'
 
-cse['host'] = approval_host['ip']
-cse['port'] = '7579'
-cse['name'] = 'Mobius'
-cse['id'] = '/Mobius2'
-cse['mqttport'] = '1883'
-cse['wsport'] = '7577'
+cse["host"] = approval_host["ip"]
+cse["port"] = '7579'
+cse["name"] = 'Mobius'
+cse["id"] = '/Mobius2'
+cse["mqttport"] = '1883'
+cse["wsport"] = '7577'
 
 # build ae
 ae_name = {}
@@ -35,22 +35,22 @@ try:
     with open('./flight.json', 'r') as f:
         ae_name = json.load(f)
 except:
-    ae_name['approval_gcs'] = 'MUV'
-    ae_name['flight'] = 'Dione'
+    ae_name["approval_gcs"] = 'MUV'
+    ae_name["flight"] = 'Dione'
     with open('./flight.json', 'w', encoding='utf-8') as f:
         json.dump(ae_name, f, indent="\t")
 
-ae['approval_gcs'] = ae_name['approval_gcs']
-ae['name'] = ae_name['flight']
+ae["approval_gcs"] = ae_name["approval_gcs"]
+ae["name"] = ae_name["flight"]
 
-ae['id'] = 'S' + ae['name']
+ae["id"] = 'S' + ae["name"]
 
-ae['parent'] = '/' + cse['name']
-ae['appid'] = str(uuid.uuid1())
-ae['port'] = '9727'
-ae['bodytype'] = 'json'  # select 'json' or 'xml' or 'cbor
-ae['tas_mav_port'] = '3105'
-ae['tas_sec_port'] = '3105'
+ae["parent"] = '/' + cse["name"]
+ae["appid"] = str(uuid.uuid1())
+ae["port"] = '9727'
+ae["bodytype"] = 'json'  # select 'json' or 'xml' or 'cbor
+ae["tas_mav_port"] = '3105'
+ae["tas_sec_port"] = '3105'
 
 # build cnt
 count = 0
@@ -107,17 +107,17 @@ count = 0
 # -------- */
 
 # build acp: not complete
-acp['parent'] = '/' + cse['name'] + '/' + ae['name']
-acp['name'] = 'acp-' + ae['name']
-acp['id'] = ae['id']
+acp["parent"] = '/' + cse["name"] + '/' + ae["name"]
+acp["name"] = 'acp-' + ae["name"]
+acp["id"] = ae["id"]
 
-conf['usesecure'] = 'disable'
+conf["usesecure"] = 'disable'
 
-if conf['usesecure'] == 'enable':
-    cse['mqttport'] = '8883'
+if conf["usesecure"] == 'enable':
+    cse["mqttport"] = '8883'
 
-conf['cse'] = cse
-conf['ae'] = ae
-conf['cnt'] = cnt_arr
-conf['sub'] = sub_arr
-conf['acp'] = acp
+conf["cse"] = cse
+conf["ae"] = ae
+conf["cnt"] = cnt_arr
+conf["sub"] = sub_arr
+conf["acp"] = acp
